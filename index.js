11a0bb549bbe86b03db622910fe3f761e128c891
Erebe <yanis.breton@hotmail.fr>
2019-03-27 19:47:11 +0100

    corrections done

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ global.events_json = {};
 
 csv().fromFile('./events.csv').then(function(jsonArrayObj){
     global.events_json = jsonArrayObj;
+    global.events_json.forEach(event => {
+        event.sin_lat = Math.sin(event.lat);
+        event.cos_lat = Math.cos(event.lat);
+    })
 })
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,47 +21,37 @@ var port = process.env.PORT || 8080;
 
 var router = express.Router();
 
-function getValue(lat, lng, name) {
-    let nearest_impression_id = null;
-    let nearest_impression_dist = null;
-    let nearest_click_id = null;
-    let nearest_click_dist = null;
-
-    events_json.forEach((elem, idx) => {
-        dist = Math.pow(lat - elem['lat'], 2) + Math.pow(lng - elem['lon'], 2);
-        
-        if (elem['event_type'] == 'imp') {
-            if (nearest_impression_id == null) {
-                nearest_impression_dist = dist;
-                nearest_impression_id = idx;
-            } else if (dist < nearest_impression_dist) {
-                nearest_impression_dist = dist;
-                nearest_impression_id = idx;
+function getValue(places) {
+    sin_lat_places = [];
+    cos_lat_places = [];
+    places.forEach(elem => {
+        elem.impressions = 0;
+        elem.clicks = 0;
+        sin_lat_places.push(Math.sin(elem.lat));
+        cos_lat_places.push(Math.cos(elem.lat));
+    });
+    events_json.forEach(event => {
+        let nearest_dist = 0;
+        let nearest_place = null;
+        places.forEach((place, idx) => {
+            let dist = 6173 * Math.acos(sin_lat_places[idx] * event.sin_lat + cos_lat_places[idx] * event.cos_lat * Math.cos(place.lon - event.lon));
+            if (nearest_place == null || dist < nearest_dist ) {
+                nearest_dist = dist;
+                nearest_place = place;
             }
-        }
-        if (elem['event_type'] == 'click') {
-            if (nearest_click_id == null) {
-                nearest_click_dist = dist;
-                nearest_click_id = idx;
-            } else if (dist < nearest_click_dist) {
-                nearest_click_dist = dist;
-                nearest_click_id = idx;
-            }
-        }
+        })
+        if (event.event_type == 'imp')
+            nearest_place.impressions += 1;
+        else if (event.event_type == 'click')
+            nearest_place.clicks += 1;
     })
-    return {
-        'lat': lat,
-        'lon': lng,
-        'name': name,
-        'impressions': nearest_impression_id,
-        'clicks': nearest_click_id
-    }
+    return (places);
 }
 
 router.put('/', function(req, res) {
     response = {}
-    req.body.forEach((elem) => {
-        response[elem.name] = getValue(elem.lat, elem.lon, elem.name);
+    getValue(req.body).forEach((elem) => {
+        response[elem.name] = elem;
     })
     res.json(response);
 });
